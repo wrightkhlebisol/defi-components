52495e47583119e36d9fafed9a599d817e79789a
Wright <calebogundiya@gmail.com>
2021-07-05 19:24:50 +0100

    Implement ERC721Holder for receiver

--- erc721-tokens/contracts/ContractA.sol
+++ erc721-tokens/contracts/ContractA.sol
@@ -1,18 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721Holder.sol';
+import '@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
-interface ContractB is ERC721Holder{
-  function deposit(uint tokenId) external;
-  function withdraw(uint tokenId) external;
+contract ContractB is ERC721Holder {
+  IERC721 public token;
+  function deposit(uint tokenId) external {
+    token.safeTransferFrom(msg.sender, address(this), tokenId);
+  }
+  function withdraw(uint tokenId) external {
+    token.safeTransferFrom(address(this), msg.sender, tokenId);
+  }
 }
 
-contract ContractA is ERC721Holder{
+contract ContractA is ERC721Holder {
   IERC721 public token;
   ContractB public contractB;
 
-  constructor(address _token){
+  constructor(address _token, address _contractB){
     token = IERC721(_token);
     contractB = ContractB(_contractB);
   }
