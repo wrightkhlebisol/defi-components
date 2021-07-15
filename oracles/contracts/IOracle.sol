93e0eee95f4c81acbdbbc6ffb6e665f64ecdc985
Wright <calebogundiya@gmail.com>
2021-07-15 04:16:24 +0100

    Creates Oracle  and Consumer contracts

---
+++ oracles/contracts/IOracle.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.3;
+
+interface IOracle {
+    function getData(bytes32 key) external view returns(bool result, uint date, uint payload);
+}
