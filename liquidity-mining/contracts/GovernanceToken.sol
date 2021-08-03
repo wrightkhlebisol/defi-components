570a8b61ec1a93ff6158257b2c4552ab491c0e16
Wright <calebogundiya@gmail.com>
2021-08-03 13:17:45 +0100

    creates governance token

---
+++ liquidity-mining/contracts/GovernanceToken.sol
@@ -0,0 +1,10 @@
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract GovernanceToken is ERC20, Ownable {
+    constructor() ERC20('Governance Token', 'GTK') Ownable(){}
+
+    function mint(address to, uint amount) external onlyOwner(){
+        _mint(to, amount);
+    }
+}
