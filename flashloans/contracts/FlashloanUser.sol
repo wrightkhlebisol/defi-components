cecd05e7f7e10bc5509d584861e70be40db93b42
Wright <calebogundiya@gmail.com>
2021-08-09 17:18:07 +0100

    Perform flashloan action and return funds

--- flashloans/contracts/FlashloanUser.sol
+++ flashloans/contracts/FlashloanUser.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.7.3;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
@@ -6,6 +8,17 @@ import './FlashloanUser.sol';
 
 contract FlashloanUser is IFlashloanUser {
     function startFlashLoan(address flashloan, uint amount, address token) external {
-        FlashloanProvider(flashloan).executeFlashloan()
+        FlashloanProvider(flashloan).executeFlashloan(
+            address(this),
+            amount,
+            token,
+            bytes('')
+        );
+    }
+
+    function flashloanCallback(uint amount, address token, bytes memory data) external override {
+        //  do some arbitrage, liuidation, etc...
+        // Reimburse borrowed tokens
+        IERC20(token).transfer(msg.sender, amount);
     }
 }
