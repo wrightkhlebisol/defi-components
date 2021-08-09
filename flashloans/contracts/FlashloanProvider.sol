35b4900f11f1c99baa873b9ddc1fbc7e5a464afc
Wright <calebogundiya@gmail.com>
2021-08-09 17:17:22 +0100

    reentrancy guard

--- flashloans/contracts/FlashloanProvider.sol
+++ flashloans/contracts/FlashloanProvider.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.3;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
 import './IFlashloanUser.sol';
 
-contract FlashloanProvider {
+contract FlashloanProvider is ReentrancyGuard{
     mapping(address => IERC20) public tokens;
 
     constructor(address[] memory _tokens){
@@ -16,7 +17,7 @@ contract FlashloanProvider {
     function executeFlashloan(
         address callback, uint amount, address _token, bytes memory data
     )
-        external
+        external nonReentrant()
     {
         IERC20 token = tokens[_token];
         uint originalBalance = token.balanceOf(address(this));
